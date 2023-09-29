b412a75a8e5b458a7fde55f89456f9e1932ab7dd
Peepss <victoordvicr@gmail.com>
2023-09-29 15:19:54 -0300

    Update - Peps #2

--- src/commands/moderation/mission.js
+++ src/commands/moderation/mission.js
@@ -1,50 +1,57 @@
 const {
-  Client,
-  Interaction,
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-  Embed,
-  EmbedBuilder,
+    Client,
+    Interaction,
+    ApplicationCommandOptionType,
+    PermissionFlagsBits,
+    EmbedBuilder,
 } = require("discord.js");
-const schema = require("../../models/schemaTeste");
-const { channel } = require("aoi.js/src/handler/slashOption");
 
 module.exports = {
-  /**
-   *
-   * @param {Client} client
-   * @param {Interaction} interaction
-   *
-   */
+    /**
+     *
+     * @param {Client} client
+     * @param {Interaction} interaction
+     *
+    */
 
-  callback: async (client, interaction) => {
-    const title = interaction.options.get("titulo");
-    const description = interaction.options.get("desc");
-    const narrador = interaction.author.username;
-    const channel = client.channels.cache.get("1156525267934781470");
+    callback: async (client, interaction) => {
+        const user = interaction.user;
+        const guild = interaction.guild;
+        const title = interaction.options.get('titulo').value;
+        const description = interaction.options.get('desc').value;
+        const imagem = interaction.options.getAttachment('imagem')
+        const channel = client.channels.cache.get("1156525267934781470");
 
-    const embed = new EmbedBuilder()
-      .setAuthor({
-        name: `${narrador}`,
-        iconURL: `${author.displayAvatarURL({ size: 1024 })}`,
-      })
-      .setTitle(`${title}`)
-      .setDescription(`${description}`);
-    id.send({ embeds: [embed] });
-  },
+        const embed = new EmbedBuilder();
+        embed.setAuthor({ name: `${user.username}`, iconURL: `${user.displayAvatarURL({ size: 1024 })}` });
+        embed.setTitle(`${title}`);
+        embed.setDescription(`${description}`);
+        if (imagem != null) {
+            embed.setImage(imagem.url)
+        };
 
-  name: "missao",
-  description: "Envia uma missão no mural.",
-  options: [
-    {
-      name: "titulo",
-      description: "Nome da missão em questão",
-      required: true,
+        channel.send({ embeds: [embed] });
     },
-    {
-      name: "desc",
-      description: "Breve resumo da missão",
-      required: true,
-    },
-  ],
+    name: 'missao',
+    description: 'Envia uma missão no mural.',
+    options: [
+        {
+            name: 'titulo',
+            description: 'Nome da missão em questão.',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'desc',
+            description: 'Breve resumo da missão.',
+            type: ApplicationCommandOptionType.String,
+            required: true
+        },
+        {
+            name: 'imagem',
+            description: 'Imagem que ilustre a missão.',
+            type: ApplicationCommandOptionType.Attachment,
+            required: false
+        }
+    ]
 };
