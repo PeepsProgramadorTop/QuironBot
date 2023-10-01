641172ddb1ee76c24420b660058612a0913c65e1
Peepss <victoordvicr@gmail.com>
2023-10-01 13:20:04 -0300

    Update Peps - #6

--- src/models/characterProfile.js
+++ src/models/characterProfile.js
@@ -1,15 +1,16 @@
 const mongoose = require("mongoose");
 
 const characterProfile = mongoose.Schema({
-    guildUser: { type: "String" }, //Id do Usuário dono do Personagem
-    guildID: { type: "String" }, //Id do Servidor
-    money: { type: Number }, //Dracmas do Personagem
-    name: { type: "String" }, //Nome do Personagem
-    avatar: { type: "String" }, //Avatar do Personagem
-    hitPoints: {
-        base: { type: Number }, //HP Base
-        current: { type: Number }, //HP Atual
-    }, //HP
+    userID: { type: "String" }, //ID do Usuário dono do Personagem
+    info: {
+        name: { type: "String" }, //Nome do Personagem
+        avatar: { type: "String" }, //Avatar do Personagem
+        money: { type: Number }, //Dracmas do Personagem
+        hitPoints: {
+            base: { type: Number }, //HP Base
+            current: { type: Number }, //HP Atual
+        }, //HP
+    },
     stats: {
         atrPoints: { type: Number }, //Pontos de Atributos, utilizados para melhorar os atributos. Podem ser ganhados em missões, eventos, treinamento, etc.
         atrCON: { type: Number }, //Atributo - Constituição
@@ -18,7 +19,6 @@ const characterProfile = mongoose.Schema({
         atrINT: { type: Number }, //Atributo - Inteligência
         atrCAR: { type: Number }, //Atributo - Carisma
     }, //Atributos do Personagem
-    inventory: { type: [mongoose.Schema.Types.Mixed] }, //Inventário do Personagem
 });
 
 module.exports = mongoose.model("characterProfile", characterProfile);
