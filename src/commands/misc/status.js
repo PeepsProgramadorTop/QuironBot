b3afefd309b8a3c620a3c1cbaafd4d5083b1cd7e
Luwpy <jpcastro.sp@gmail.com>
2023-10-03 02:08:27 -0300

    Comando status OK

--- src/commands/misc/status.js
+++ src/commands/misc/status.js
@@ -1,46 +1,75 @@
 const {
-    ApplicationCommandOptionType,
-    PermissionFlagsBits,
-    EmbedBuilder,
-    ActionRowBuilder,
-    StringSelectMenuBuilder,
-    StringSelectMenuOptionBuilder
+  ApplicationCommandOptionType,
+  PermissionFlagsBits,
+  EmbedBuilder,
+  ActionRowBuilder,
+  StringSelectMenuBuilder,
+  StringSelectMenuOptionBuilder,
+  ComponentType,
 } = require("discord.js");
 const characterProfile = require("../../models/characterProfile");
 
-
 module.exports = {
+  callback: async (client, interaction) => {
+    const user = interaction.user;
+    const characterGroup = await characterProfile.find({ userID: user.id });
 
-    callback: async (client, interaction) => {
-        const user = interaction.user;
-        const characterGroup = await characterProfile.find({ userID: user.id });
+    const names = [];
+    characterGroup.forEach((data) => {
+      names.push({ name: `${data.info.name}` });
+    });
 
-        const names = [];
-        characterGroup.forEach((data) => {
-            names.push({ name: `${data.info.name}`});
-        });
+    const charSelectMenu = new StringSelectMenuBuilder()
+      .setCustomId(interaction.id)
+      .setPlaceholder("Selecione o personagem que você quer ver o status.")
+      .setMinValues(0)
+      .setMaxValues(1)
+      .addOptions(
+        names.map((characters) =>
+          new StringSelectMenuOptionBuilder()
+            .setLabel(characters.name)
+            .setDescription("Personagem.")
+            .setValue(characters.name)
+        )
+      );
 
-        const charSelectMenu = new StringSelectMenuBuilder()
-            .setCustomId(interaction.id)
-            .setPlaceholder("Selecione o personagem que você quer ver o status.")
-            .setMinValues(0)
-            .setMaxValues(1)
-            .addOptions(
-                names.map((characters) => 
-                    new StringSelectMenuOptionBuilder()
-                        .setLabel(characters.name)
-                        .setDescription("Personagem.")
-                        .setValue(characters.name.toLowerCase().replace(' ', '_'))
-                )
-            );
+    const actionRow = new ActionRowBuilder().addComponents(charSelectMenu);
 
-        const actionRow = new ActionRowBuilder().addComponents(charSelectMenu);
-        
-        interaction.reply({
-            components: [actionRow]
-        });
-    },
+    const reply = await interaction.reply({
+      components: [actionRow],
+    });
 
-    name: "status",
-    description: "Pega os personagem aí doidão"
+    const collector = reply.createMessageComponentCollector({
+      componentType: ComponentType.StringSelect,
+      filter: (i) =>
+        i.user.id === interaction.user.id && i.customId === interaction.id,
+      time: 60_000,
+    });
+
+    collector.on("collect", async (interaction) => {
+      const character = interaction.values[0];
+
+      const query = await characterProfile.findOne({
+        userID: user.id,
+        "info.name": character,
+      });
+
+      const embed = new EmbedBuilder()
+        .setTitle(`${query.info.name}`)
+        .setDescription(
+          `FORÇA💪: ${query.stats.atrFOR}
+        DESTREZA: ${query.stats.atrAGI}
+        CONSTITUIÇÃO: ${query.stats.atrCON}
+        INTELIGENCIA: ${query.stats.atrINT}
+        SABEDORIA: ${query.stats.atrSAB}
+        CARISMA: ${query.stats.atrCAR}`
+        )
+        .setThumbnail(query.info.avatar);
+
+      interaction.reply({ embeds: [embed] });
+    });
+  },
+
+  name: "status",
+  description: "Pega os personagem aí doidão",
 };
