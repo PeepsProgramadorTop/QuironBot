ef7335cc64ee51099aa885a14f3c7e747e916063
Peepss <victoordvicr@gmail.com>
2023-10-02 23:56:54 -0300

    Update Peps - #8

--- src/commands/misc/status.js
+++ src/commands/misc/status.js
@@ -1,39 +1,46 @@
 const {
-  Client,
-  Interaction,
-  ApplicationCommandOptionType,
-  PermissionFlagsBits,
-  EmbedBuilder,
+    ApplicationCommandOptionType,
+    PermissionFlagsBits,
+    EmbedBuilder,
+    ActionRowBuilder,
+    StringSelectMenuBuilder,
+    StringSelectMenuOptionBuilder
 } = require("discord.js");
+const characterProfile = require("../../models/characterProfile");
+
 
 module.exports = {
-  /**
-   *
-   * @param {Client} client
-   * @param {Interaction} interaction
-   *
-   */
 
-  callback: async (client, interaction) => {
-    const character = require("../../models/characterProfile");
-    const user = interaction.user;
-    const characterGroup = await character.find({ userID: user.id });
-    let names = [];
-    let choices = [];
+    callback: async (client, interaction) => {
+        const user = interaction.user;
+        const characterGroup = await characterProfile.find({ userID: user.id });
 
-    characterGroup.forEach((data) => {
-      names.push(data.info.name);
-    });
+        const names = [];
+        characterGroup.forEach((data) => {
+            names.push({ name: `${data.info.name}`});
+        });
 
-  },
+        const charSelectMenu = new StringSelectMenuBuilder()
+            .setCustomId(interaction.id)
+            .setPlaceholder("Selecione o personagem que você quer ver o status.")
+            .setMinValues(0)
+            .setMaxValues(1)
+            .addOptions(
+                names.map((characters) => 
+                    new StringSelectMenuOptionBuilder()
+                        .setLabel(characters.name)
+                        .setDescription("Personagem.")
+                        .setValue(characters.name.toLowerCase().replace(' ', '_'))
+                )
+            );
 
-  name: "status",
-  description: "Pega os personagem aí doidão",
-  option: [
-    {
-      name: "Personagem",
-      description: "Escolha o personagem",
-      choices: [],
+        const actionRow = new ActionRowBuilder().addComponents(charSelectMenu);
+        
+        interaction.reply({
+            components: [actionRow]
+        });
     },
-  ],
+
+    name: "status",
+    description: "Pega os personagem aí doidão"
 };
