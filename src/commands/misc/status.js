1ee6d3e1bece038b989f10ae90049495d6ad1ba4
Peepss <victoordvicr@gmail.com>
2023-10-07 03:20:42 -0300

    Update Peps - #13

--- src/commands/misc/status.js
+++ src/commands/misc/status.js
@@ -12,6 +12,18 @@ const characterProfile = require("../../models/characterProfile");
 const axios = require('axios');
 const sharp = require('sharp');
 const Canvas = require('@napi-rs/canvas');
+const { join } = require('path')
+
+const applyText = (canvas, text) => {
+	const context = canvas.getContext('2d');
+	let fontSize = 42;
+
+	do {
+		context.font = `${fontSize -= 10}px Windlass`;
+	} while (context.measureText(text).width > canvas.width - 400);
+
+	return context.font;
+};
 
 module.exports = {
     callback: async (client, interaction) => {
@@ -59,37 +71,75 @@ module.exports = {
                 "info.name": character,
             });
 
+            // ------------------------------------
+            // Abaixo se inicia a criação do banner!
+            // ------------------------------------
+
             const bannerURL = query.info.banner;
+            const characterAvatarURL = query.info.avatar;
             const response = await axios.get(bannerURL, { responseType: 'arraybuffer' });
             const imageBuffer = Buffer.from(response.data);
+            Canvas.GlobalFonts.registerFromPath(join(__dirname, '../..', 'fonts', 'windlass.ttf'), 'Windlass');
 
-            // Redimensiona o GIF usando gif-encoder-2
 
-            const bannerResized = await sharp(imageBuffer)
-                .resize(700, 250)
+            //Separação - Das consts default para a criação do background.
+
+            const resizedBanner = await sharp(imageBuffer)
+                .resize(1079, 353)
                 .toBuffer();
-            const canvas = Canvas.createCanvas(700, 250);
-            const context = canvas.getContext('2d');
 
-            const background = await Canvas.loadImage(bannerResized);
-            const symbol = await Canvas.loadImage('https://cdn.discordapp.com/attachments/1158781690517393418/1158790947069509652/image.png?ex=652eab2b&is=651c362b&hm=43916bbe3e8a1f522461ce3dafdd190cb6678b7500c2a9115797dd668f498c61&');
+            const backgroundCanvas = Canvas.createCanvas(1079, 486);
+            const backgroundContext = backgroundCanvas.getContext('2d');
 
-            // This uses the canvas dimensions to stretch the image onto the entire canvas
-            context.drawImage(background, 0, 0, canvas.width, canvas.height);
-            context.drawImage(symbol, 20, 170, 60, 60);
-            context.drawImage(symbol, 100, 170, 60, 60);
-            context.drawImage(symbol, 180, 170, 60, 60);
+            const background = await Canvas.loadImage(resizedBanner);
 
-            const resizedBuffer = await canvas.encode('png')
+            backgroundContext.fillStyle = '#03a9f4'
+            backgroundContext.beginPath();
+            backgroundContext.roundRect(0, 0, 1079, 400, 42);
+            backgroundContext.closePath();
+            backgroundContext.clip();
 
-            // Crie um objeto de anexo com o GIF redimensionado
+            backgroundContext.drawImage(background, 0, 0, 1079, 353);
+
+            const bannerBackground = backgroundCanvas.toBuffer('image/png');
+
+            //Separação - Da criação do background para a layer superior do banner.
+
+            const layerCanvas = Canvas.createCanvas(1079, 486);
+            const layerContext = layerCanvas.getContext('2d');
+
+            const backgroundBuffer = await Canvas.loadImage(bannerBackground);
+            const bannerLayer = await Canvas.loadImage('./src/images/banner_layer.png');
+            const characterAvatar = await Canvas.loadImage(characterAvatarURL);
+
+            layerContext.drawImage(backgroundBuffer, 0, 0, layerCanvas.width, layerCanvas.height);
+            layerContext.drawImage(bannerLayer, 0, 0, layerCanvas.width, layerCanvas.height);
+            layerContext.font = applyText(layerCanvas, `${query.info.name}`);
+            layerContext.fillStyle = '#FFFFFF';
+            layerContext.fillText(`${query.info.name}`, 290, 418);
+            layerContext.font = '29px Calibri';
+            layerContext.fillStyle = '#4E4F54';
+            layerContext.fillText(`@${user.username}`, 290, 443);
+
+            layerContext.beginPath();
+            layerContext.arc(146, 348, 108, 0, Math.PI * 2, true);
+            layerContext.closePath();
+            layerContext.clip();
+            layerContext.drawImage(characterAvatar, 38, 240, 216, 216);
+
+            const resizedBuffer = await layerCanvas.encode('png');
+
             const attachment = new AttachmentBuilder(resizedBuffer, { name: `banner.png` });
 
-            const bannerEmbed = new EmbedBuilder()
-                .setAuthor({ name: `Interpretado por: ${user.username}`, iconURL: user.displayAvatarURL({ size: 1024 }) })
-                .setTitle(`Status de ${query.info.name}`)
+            // ------------------------------------
+            // Aqui termina-se a criação do banner e se inicia a criação e o processo de enviar as embeds.
+            // ------------------------------------
+
+            const firstEmbed = new EmbedBuilder()
+                .setAuthor({ name: `Quíron lhe apresenta...`, iconURL: 'https://i.imgur.com/3LUWvgi.png' })
+                .setTitle(`📂  —  Arquivo de Semideus(a)︰`)
                 .setImage(`attachment://banner.png`);
-            const embed = new EmbedBuilder()
+            const secondEmbed = new EmbedBuilder()
                 .setDescription(`\`\`\`GERAL:\`\`\`
 ❤️<:dot:1158109856725733378>**Pontos de Vida︰** \`${query.info.hitPoints.current}/${query.info.hitPoints.base}HP\`
 🪙<:dot:1158109856725733378>**Dracmas︰** \`×${query.info.money}\`
@@ -107,10 +157,9 @@ module.exports = {
 🧠<:dot:1158109856725733378>**Inteligência (INT)︰** \`×${query.stats.atrINT}\`
 ✨<:dot:1158109856725733378>**Carisma (CAR)︰** \`×${query.stats.atrCAR}\`
 `)
-                .setThumbnail(query.info.avatar)
                 .setImage("https://i.imgur.com/rg4KxSi.png");
 
-            reply.edit({ embeds: [bannerEmbed, embed], files: [attachment], components: [] });
+            reply.edit({ embeds: [firstEmbed, secondEmbed], files: [attachment], components: [] });
         });
     },
 
